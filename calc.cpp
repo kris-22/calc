7e64ed308895eb2ad23a6aa81352e1180d7e27ca
Krzysztof Bielecki <51539457+kris-22@users.noreply.github.com>
2022-03-08 15:42:35 +0100

    proba ze stosem

--- calc.cpp
+++ calc.cpp
@@ -2,9 +2,10 @@
 #include <string.h>
 using namespace std;
 string dzialanie;
-short znaki[20] = {0}; //przechowuje działanie podzielone na pojedyńcze znaki
-short d = 0; //zmienna operacyjna dla pędtli - |nadawanie piorytetu dla operacji matematycznej|
-int bufor=0; //zmienna operacyjna dla pędtli - |sortowanie piorytetu operacji matematycznej|
+short znaki[20] = {0}; //przechowuje znaki aperacji matematycznej
+short d = 0; //zmienna operacyjna dla pętli - |nadawanie piorytetu dla operacji matematycznej|
+int bufor=0; //zmienna operacyjna dla pętli - |sortowanie piorytetu operacji matematycznej| - przechowuje numer tablicy 
+int stos = -1;
 short piorytet[20] = {0}; //tablica piorytetu operacji matematycznej
 int main()
 {
@@ -19,21 +20,75 @@ int main()
     }
     cout << "tab\n";
     //sortowanie piorytetu operacji matematycznej
-    for (int x = 0; x < dzialanie.length(); x++){
+    for (int x = 0; x < dzialanie.length(); x++) {
         for (int i = 0; i < dzialanie.length(); i++) {
-            if (znaki[i] > bufor) {
+            if (znaki[i] > znaki[bufor]) {
                 bufor = i;
             }
         }
         cout << znaki[bufor] << ",";
-        piorytet[x] = znaki[bufor];
+        piorytet[x] = bufor;
+        // cout << piorytet[x] << ",piorytet\n";
         znaki[bufor] = 0;
         bufor = 0;
     }
+    cout << dzialanie[0] << dzialanie[1] << dzialanie[2] << dzialanie[3] << "\n";
+    cout << piorytet[0] << piorytet[1] << piorytet[2] << piorytet[3] << "\n";
+    int x = 0;
+    for (int i = 0; i < dzialanie.length(); i++) {
+        if (piorytet[i] > 0) {
+            cout << piorytet[i] << ",piorytet\n";
+            cout << dzialanie[piorytet[i]] << ",dzialanie\n";
 
-  /*  for (int i = 0; i < dzialanie.length(); i++) {
-        stos[i]
-    }*/
+
+//_________________________________________________
+            //stos do naprawy
+
+
+            switch (dzialanie[piorytet[i]])
+            {
+            case '+':
+                if (stos > -1) {
+                    stos = stos + (dzialanie[piorytet[i] + 1] - 48);
+                }
+                else {
+                    stos = (dzialanie[piorytet[i] - 1]-48) + (dzialanie[piorytet[i] + 1]-48);
+                    cout << dzialanie[piorytet[i] - 1] << " +" << dzialanie[piorytet[i] + 1]<<"=" << stos;
+                }
+                break;
+            case '-':
+                if (stos > -1) {
+                    stos = stos - (dzialanie[piorytet[i] + 1]-48);
+                }
+                else {
+                    stos = (dzialanie[piorytet[i] - 1]-48) - (dzialanie[piorytet[i]+1]-48);
+                }
+                break;
+            case '*':
+                if (stos > -1) {
+                    stos = stos * (dzialanie[piorytet[i] + 1] - 48);
+                }
+                else {
+                    stos = (dzialanie[piorytet[i] - 1] - 48) * (dzialanie[piorytet[i] + 1] - 48);
+                }
+                break;
+            case '/':
+                if (stos > -1) {
+                    stos = stos / (dzialanie[piorytet[i] + 1] - 48);
+                }
+                else {
+                    stos = (dzialanie[piorytet[i] - 1] - 48) / (dzialanie[piorytet[i] + 1] - 48);
+                }
+                break;
+            }
+
+            cout <<"stos" << stos <<"\n";
+           
+        }       
+        
+    }
+
+    cout << "\n" << stos << ",";
 
 }
 
