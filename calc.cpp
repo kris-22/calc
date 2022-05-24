13d200ae9610b27042a6ed4e2503408d92e151ed
Krzysztof Bielecki <51539457+kris-22@users.noreply.github.com>
2022-05-24 02:17:37 +0200

    wersja na zajęcia

--- calc.cpp
+++ calc.cpp
@@ -6,13 +6,12 @@ bool czyPusty(double* sstos) {
     if (sstos[0] == 0)return true; else return false;
 }
 bool czyPelny(double* sstos) {
-    if (sstos[0] == 20) return true; else return false;
+    if (sstos[0] == 100) return true; else return false;
 }
 void naStos(double x, double* sstos) {
     if (!czyPelny(sstos)) {
         sstos[0]++;
         sstos[(int)sstos[0]] = x;
-        //cout <<"dodano: " << x << " na stos"<<endl;
     }
     else {
         cout << "stos pelny\n";
@@ -30,7 +29,6 @@ double zeStosu(double* sstos) {
 }
 double rozpalStos(double* sstos) {
     if (!czyPusty(sstos)) {
-        //cout <<"rozpalono" << sstos[(int)sstos[0]] << endl;
         return sstos[(int)sstos[0]];
     }
     else {
@@ -39,103 +37,88 @@ double rozpalStos(double* sstos) {
     }
 }
 //-----------------------------------------╝
+
 // przekształca działanie na onp
-//pobiera działanie matematyczne z konsoli
 string toOnp(string dzialanie) {
 
-    int znaki[20] = { 0 }; //przechowuje znaki aperacji matematycznej
+    int znaki[100] = { 0 }; //przechowuje znaki aperacji matematycznej
     short d = 0; //zmienna operacyjna dla pętli - |nadawanie piorytetu dla operacji matematycznej|
     short bufor = 0; //zmienna operacyjna dla pętli - |sortowanie piorytetu operacji matematycznej| - przechowuje numer tablicy 
-    short piorytet[20] = { 0 }; //tablica piorytetu operacji matematycznej wskazuje na numer miejsce w tablicy
+    short piorytet[100] = { 0 }; //tablica piorytetu operacji matematycznej wskazuje na numer miejsce w tablicy
     string wynik = "";
     string poczekalnia = "";
 
     //nadawanie piorytetu dla operacji matematycznej
-    for (int i = 0; i < dzialanie.length(); i++) {
+    for (short i = 0; i < dzialanie.length(); i++) {
         if (dzialanie[i] == '+' || dzialanie[i] == '-') znaki[i] = 1 + d;
         if (dzialanie[i] == '*' || dzialanie[i] == '/') znaki[i] = 2 + d;
         if (dzialanie[i] == '(') d += 10;
         if (dzialanie[i] == ')') d += -10;
-         cout << znaki[i] << ",";
     }
-    // cout << endl;
 
     //sortowanie piorytetu operacji matematycznej
-    for (int x = 0; x < dzialanie.length(); x++) {
-        for (int i = 0; i < dzialanie.length(); i++) {
+    for (short x = 0; x < dzialanie.length(); x++) {
+        for (short i = 0; i < dzialanie.length(); i++) {
             if (znaki[i] > znaki[bufor]) {
                 bufor = i;
             }
         }
-        //cout << znaki[bufor] << ",";
         piorytet[x] = bufor;
         znaki[bufor] = 0;
         bufor = 0;
     }
 
     //zapisz w onp
-    for (int i = 0; i < dzialanie.length(); i++) {
-         cout << piorytet[i] <<","<<dzialanie[piorytet[i]]<< endl;
-
+    for (short i = 0; i < dzialanie.length(); i++) {
         if (piorytet[i] > 0) {
             if (dzialanie[piorytet[i] + 1] == '(' && dzialanie[piorytet[i] - 1] != ')' && dzialanie[piorytet[i] - 1] != '(' && dzialanie[piorytet[i] - 1] != '!') {
 
                 poczekalnia += dzialanie[piorytet[i] - 1];
-                dzialanie[piorytet[i] - 1] = '!'; //onacz liczbę jako wykorzystaną
+                dzialanie[piorytet[i] - 1] = '!'; //oznacz liczbę jako wykorzystaną
             }
             if (dzialanie[piorytet[i] - 1] != ')' && dzialanie[piorytet[i] - 1] != '(' && dzialanie[piorytet[i] - 1] != '!')
             {
                 wynik += dzialanie[piorytet[i] - 1];
-                dzialanie[piorytet[i] - 1] = '!'; //onacz liczbę jako wykorzystaną 
+                dzialanie[piorytet[i] - 1] = '!'; //oznacz liczbę jako wykorzystaną 
             }
             if (dzialanie[piorytet[i] + 1] != '(' && dzialanie[piorytet[i] + 1] != ')' && dzialanie[piorytet[i] + 1] != '!')
             {
                 wynik += dzialanie[piorytet[i] + 1];
-                dzialanie[piorytet[i] + 1] = '!'; //onacz liczbę jako wykorzystaną
+                dzialanie[piorytet[i] + 1] = '!'; //oznacz liczbę jako wykorzystaną
             }
-            wynik = poczekalnia+ wynik+ dzialanie[piorytet[i]];
+            wynik = poczekalnia + wynik + dzialanie[piorytet[i]];
             poczekalnia = "";
         }
     }
-    //wynik = poczekalnia + wynik;
     return wynik;
-
 }
-//wykonuje obliczenia wykorzystujać ONP
+
+ //wykonuje obliczenia wykorzystujac ONP
 double licz(string onpString, double* sstos) {
-    for (int i = 0; i < onpString.length(); i++) {
+    for (short i = 0; i < onpString.length(); i++) {
 
         if (onpString[i] != '-' && onpString[i] != '+' && onpString[i] != '/' && onpString[i] != '*') {
             onpString[i] -= 48;
             naStos(onpString[i], sstos);
         }
         else {
-            // cout << "Licze" <<endl;
             double b = zeStosu(sstos);
             double a = zeStosu(sstos);
             switch (onpString[i]) {
             case  '+':
-                // cout << onpString[i] << endl;
                 naStos(a + b, sstos);
-                // cout << rozpalStos(sstos) << endl;
 
                 break;
             case '-':
-                // cout << onpString[i] << endl;
                 naStos(a - b, sstos);
-                // cout << rozpalStos(sstos) << endl;
 
                 break;
             case '*':
-                //cout << onpString[i] << endl;
                 naStos(a * b, sstos);
-                //cout << rozpalStos(sstos) << endl;
 
                 break;
             case '/':
-                //cout << onpString[i] << endl;
                 naStos(a / b, sstos);
-                //cout << rozpalStos(sstos) << endl;
                 break;
             }
         }
@@ -143,27 +126,21 @@ double licz(string onpString, double* sstos) {
     return rozpalStos(sstos);
 }
 
-
 int main()
 {
-    //string dzialanie = "2-2+3+(2+2)-2";
-    //string dzialanie = "(2+3)*(3*(4+6))";
-    string dzialanie = "((1+1)+3)-(9+1-2)";
-    // string dzialanie = "(2+3)-8";
-        // przechowuje działanie patematyczne
-        //string dzialanie = "2+2*(3/(2+4)/2)";
+    // przechowuje działanie matematyczne
+    string dzialanie;
+    dzialanie = "((1+1)+3)-(9+1-2)";
 
-        cin >> dzialanie; 
-    cout << endl << dzialanie << endl;
+    //cin >> dzialanie;
+    //cout << endl << dzialanie << endl;
+
+    //wyświetl i zapisz działanie jako ONP
     cout << endl << "zapis onp : " << toOnp(dzialanie) << endl;
 
-
-
-    double stos1[11] = { 0 }; //tworzenie nowego stosu
+    double stos1[100] = { 0 }; //tworzenie nowego stosu
     stos1[0] = 0; //pływak stosu
+
+    //wyświetl wynik
     cout << endl << "wynik: " << licz(toOnp(dzialanie), stos1) << endl;
 }
-
-
-
-//dzialanie[piorytet[i] - 1 = ")"
